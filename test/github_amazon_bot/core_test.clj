b566b3788690aaa6da91d7702a31aa3e0611561f
ayush.sachdeva <ayush.sachdeva@helpshift.com>
2020-07-07 15:48:46 +0530

    Initial commit

---
+++ test/github_amazon_bot/core_test.clj
@@ -0,0 +1,7 @@
+(ns github-amazon-bot.core-test
+  (:require [clojure.test :refer :all]
+            [github-amazon-bot.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
