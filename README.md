c337a483fd560019a178ef4f1d37720c305f2d42
ayush.sachdeva <ayush.sachdeva@helpshift.com>
2020-07-08 16:18:51 +0530

    Run every 5 mins, minor fixes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # github-amazon-bot
 
-Checks this [amazon link](https://www.amazon.in/Apple-iPad-11-inch-Wi-Fi-128GB/dp/B0864JKY83) if iPad is available for purchase. If available, I'll receive a message over telegram. This is scheduled every 20 minutes via github actions.
+Checks this [amazon link](https://www.amazon.in/Apple-iPad-11-inch-Wi-Fi-128GB/dp/B0864JKY83) if iPad is available for purchase. If available, I'll receive a message over telegram. This is scheduled every 5 minutes via github actions.
--- src/github_amazon_bot/core.clj
+++ src/github_amazon_bot/core.clj
@@ -13,11 +13,11 @@
   [text-to-search contents]
   (let [pattern (re-pattern (str "(?im)" text-to-search))
         first-occurence (re-find pattern contents)
-        num-occurrences (count first-occurence)]
-    (if (> num-occurrences 0) true false)))
+        num-chars (count first-occurence)]
+    (> num-chars 0)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Main function"
   []
   (let [url "https://www.amazon.in/Apple-iPad-11-inch-Wi-Fi-128GB/dp/B0864JKY83"
         text-to-search "currently unavailable"
