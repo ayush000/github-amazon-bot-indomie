39dfe3169005b233e894db84534cd68453ec7746
ayush.sachdeva <ayush.sachdeva@helpshift.com>
2020-07-10 15:08:14 +0530

    Implement suggested improvements

--- src/github_amazon_bot/core.clj
+++ src/github_amazon_bot/core.clj
@@ -2,8 +2,6 @@
   (:require [clj-http.client :as client]
             [environ.core :refer [env]]
             [morse.api :as api]))
-(def ipads [{:url "https://www.amazon.in/dp/B0864JNFB8/" :name "iPad Pro 12.9"}
-            {:url "https://www.amazon.in/Apple-iPad-11-inch-Wi-Fi-128GB/dp/B0864JKY83" :name "iPad Pro 11"}])
 
 (defn get-site-body
   "Get website body"
@@ -26,13 +24,12 @@
         text-to-search "currently unavailable"
         token (env :telegram-token)
         chat-id (env :telegram-chat-id)]
-    (dorun
-     (for [ipad ipads :let [name (ipad :name)
-                            url (ipad :url)
-                            body (get-site-body url)
-                            ipad-is-available (not (is-text-present? text-to-search body))]]
-       (if ipad-is-available
-         (try
-           (api/send-text token chat-id (str name " is available! Please buy it from " url))
-           (catch Exception ex true)) true)))))
+    (doseq [ipad ipads :let [name (ipad :name)
+                             url (ipad :url)
+                             body (get-site-body url)
+                             ipad-is-available (not (is-text-present? text-to-search body))]]
+      (when ipad-is-available
+        (try
+          (api/send-text token chat-id (str name " is available! Please buy it from " url))
+          (catch Exception))))))
 
