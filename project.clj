2deef1fede999bbe9903fc4177c5686ac2e645ea
ayush.sachdeva <ayush.sachdeva@helpshift.com>
2020-07-10 16:07:05 +0530

    Fetch shopping list from a JSON file

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
                  [org.clojure/clojure "1.10.1"]
                  [morse "0.4.3"]
                  [clj-http "3.10.1"]
-                 [environ "1.2.0"]]
+                 [environ "1.2.0"]
+                 [cheshire "5.10.0"]]
   :main ^:skip-aot github-amazon-bot.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/github_amazon_bot/core.clj
+++ src/github_amazon_bot/core.clj
@@ -1,7 +1,8 @@
 (ns github-amazon-bot.core
   (:require [clj-http.client :as client]
             [environ.core :refer [env]]
-            [morse.api :as api]))
+            [morse.api :as api]
+            [cheshire.core :as cc]))
 
 (defn get-site-body
   "Get website body"
@@ -16,20 +17,25 @@
         num-chars (count first-occurence)]
     (> num-chars 0)))
 
+(defn get-items-to-watch
+  "Get list of items to watch from a file"
+  []
+  (let [file-contents (slurp "shopping_list.json")]
+    (cc/parse-string file-contents true)))
+
 (defn -main
   "Main function"
   []
-  (let [ipads [{:url "https://www.amazon.in/dp/B0864JNFB8/" :name "iPad Pro 12.9"}
-               {:url "https://www.amazon.in/Apple-iPad-11-inch-Wi-Fi-128GB/dp/B0864JKY83" :name "iPad Pro 11"}]
+  (let [items (get-items-to-watch)
         text-to-search "currently unavailable"
         token (env :telegram-token)
         chat-id (env :telegram-chat-id)]
-    (doseq [ipad ipads :let [name (ipad :name)
-                             url (ipad :url)
+    (doseq [item items :let [name (item :name)
+                             url (item :url)
                              body (get-site-body url)
-                             ipad-is-available (not (is-text-present? text-to-search body))]]
-      (when ipad-is-available
+                             item-is-available (not (is-text-present? text-to-search body))]]
+      (when item-is-available
         (try
           (api/send-text token chat-id (str name " is available! Please buy it from " url))
-          (catch Exception))))))
+          (catch Exception ex))))))
 
